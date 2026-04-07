9e96caf5722c0c7d990060a3e3419787b91a6dfe
mohamed <ivend@mobica.net>
2026-04-07 12:24:25 +0200

    feat: implement NotificationViewSet for authenticated user notifications and read status updates

--- notifications/views.py
+++ notifications/views.py
@@ -10,6 +10,9 @@ class NotificationViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return Notification.objects.filter(user=self.request.user).order_by('-created_at')
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
     @decorators.action(detail=True, methods=['post'])
     def mark_read(self, request, pk=None):
         notification = self.get_object()
